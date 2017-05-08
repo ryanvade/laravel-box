25a054aa318d67b9493adc466f9deb793c34906a
Ryan Owens <RyanOwens@linux.com>
2017-05-08 16:21:57 -0500

    Initial DownloadFilecommand

---
+++ src/LaravelBox/Commands/Files/DownloadFileCommand.php
@@ -0,0 +1,42 @@
+<?php
+
+use GuzzleHttp\Psr7\Response;
+
+namespace LaravelBox\Commands\Files;
+
+class GetFileInformationCommand extends AbstractFileCommand
+{
+    private $storageLocation;
+    public function __construct(string $token, string $fileId, string $storageLocation)
+    {
+        parent::__construct($token, $fileId);
+        $this->storageLocation = $storageLocation;
+    }
+
+    private function execute()
+    {
+        $url = "https://api.box.com/2.0/files/${$this->fileId}/content";
+        $options = [
+          'sink' => $this->storageLocation,
+          'headers' => [
+            'Authorization' => "Bearer ${$this->token}",
+          ],
+        ];
+        $client = parent::getInstance();
+        return $client->get($url, $options);
+    }
+
+    public function getResponse()
+    {
+        $resp = $this->execute();
+        $status = [];
+        if (! $resp instanceof Response) {
+            $status['success'] = 'error';
+        } else {
+            $status['success'] = $resp->getReasonPhrase();
+            $status['status-code'] = $resp->getStatusCode();
+            $status['body'] = $resp->getBody();
+        }
+        return $status;
+    }
+}
