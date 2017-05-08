3e6691b5a2e98a942c1cc77aac3f152ec2a705c1
Ryan Owens <RyanOwens@linux.com>
2017-05-08 17:18:34 -0500

    Download File using File resource

--- src/LaravelBox/Commands/Files/DownloadFileCommand.php
+++ src/LaravelBox/Commands/Files/DownloadFileCommand.php
@@ -16,16 +16,21 @@ class GetFileInformationCommand extends AbstractFileCommand
 
     private function execute()
     {
-        $url = "https://api.box.com/2.0/files/${$this->fileId}/content";
-        $options = [
-          'sink' => $this->storageLocation,
-          'headers' => [
-            'Authorization' => "Bearer ${$this->token}",
-          ],
-        ];
-        $client = parent::getInstance();
+        try {
+            $url = "https://api.box.com/2.0/files/${$this->fileId}/content";
+            $handle = fopen($this->storageLocation, 'w');
+            $options = [
+              'sink' => $handle,
+              'headers' => [
+                'Authorization' => "Bearer ${$this->token}",
+              ],
+            ];
+            $client = parent::getInstance();
 
-        return $client->get($url, $options);
+            return $client->get($url, $options);
+        } catch (Exception $e) {
+            return false;
+        }
     }
 
     public function getResponse()
